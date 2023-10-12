d7198cf9bf87715ecee2efdea32f9210847989c0
hytm <nico.frw@gmail.com>
2023-10-12 09:51:11 +0200

    init

---
+++ backend/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+
+	"github.com/Hytm/demo-app-ws/backend/pkg/websocket"
+)
+
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	ws, err := Upgrade(w, r)
+	if err != nil {
+		fmt.Fprintf(w, "%+V\n", err)
+	}
+	go websocket.Writer(ws)
+	websocket.Reader(ws)
+}
+
+func setupRoutes() {
+	http.HandleFunc("/ws", serveWs)
+}
+
+func main() {
+	fmt.Println("Distributed Chat App v0.01")
+	setupRoutes()
+	http.ListenAndServe(":8080", nil)
+}

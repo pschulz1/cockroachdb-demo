5074ff5139bf756f6a644ae1e1135a7b5fc92df8
hytm <nico.frw@gmail.com>
2023-10-12 17:40:25 +0200

    update sql file

--- sql/init.sql
+++ sql/init.sql
@@ -8,4 +8,6 @@ CREATE TABLE orders (
     currency STRING,
     number_of_items int,
     country STRING
-);
+) WITH (ttl_expire_after = '5 minutes', ttl_job_cron = '*/1 * * * *');
+
+SET cluster setting kv.rangefeed.enabled = true;

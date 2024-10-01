7a0fd55b49107a0726dde69f27704e606ce87851
pschulz1 <patrickschulz1@outlook.com>
2024-10-01 08:37:41 +0200

    some cleanup

--- scripts/99_clean.sh
+++ scripts/99_clean.sh
@@ -8,3 +8,6 @@ rm -rf node1 node2 node3 node4 node5
 
 #Stop CRDB running haproxy
 for pid in $(ps -ef | grep "haproxy" | grep -v grep | awk '{print $2}'); do kill -9 $pid; done
+
+#Remove main
+rm ../main

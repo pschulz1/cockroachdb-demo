a1f826c88ceda05dfd4a6dd1359097e77b3ddede
pschulz1 <patrickschulz1@outlook.com>
2024-10-02 11:50:25 +0200

    Adding randomness to the node failure

--- scripts/03_kill.sh
+++ scripts/03_kill.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-#Stop CRDB running processes for node 4
-for pid in $(ps -ef | grep "listen-addr=localhost:26260" | grep -v grep | awk '{print $2}'); do
+numbers=(57 58 59 60 61)
+random_index=$((RANDOM % ${#numbers[@]}))
+random_number=${numbers[$random_index]}
+
+#Stop CRDB running processes for a random node
+for pid in $(ps -ef | grep "listen-addr=localhost:262$random_number" | grep -v grep | awk '{print $2}'); do
     now=$(date +"%T")
-    echo "Kill a node at $now"
+    echo "Kill node on port 262$random_number at $now"
     kill -9 $pid; 
 done
+
+echo 262$random_number > restore.txt
--- scripts/04_restore.sh
+++ scripts/04_restore.sh
@@ -1,10 +1,44 @@
 #!/bin/bash
 
+file=restore.txt
+port=$(< "$file")
+now=$(date +"%T")
+
+echo "Restarting node on port $port at $now"
+
+case $port in
+  26257)
+    node_ui_port=8080
+    node="node1"
+    ;;
+  26258)
+    node_ui_port=8081
+    node="node2"
+    ;;
+  26259)
+    node_ui_port=8082
+    node="node3"
+    ;;
+  26260)
+    node_ui_port=8083
+    node="node4"
+    ;;
+  26261)
+    node_ui_port=8084
+    node="node5"
+    ;;
+  *)
+    error_string="Invalid input"
+    ;;
+esac
+
+# echo $node
+
 cockroach start \
 --insecure \
---store=node4 \
---listen-addr=localhost:26260 \
---advertise-addr=localhost:26260 \
---http-addr=localhost:8083 \
+--store=$node \
+--listen-addr=localhost:$port \
+--advertise-addr=localhost:$port \
+--http-addr=localhost:$node_ui_port \
 --join=localhost:26257,localhost:26258,localhost:26259 \
 --background

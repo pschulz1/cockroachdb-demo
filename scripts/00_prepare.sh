945e298425ebb1a47a2c630829ed1d1b4fdd7f94
pschulz1 <50155921+pschulz1@users.noreply.github.com>
2026-01-09 09:01:32 +0100

    Update 00_prepare.sh

--- scripts/00_prepare.sh
+++ scripts/00_prepare.sh
@@ -11,8 +11,8 @@ haproxy -f ../haproxy/haproxy.cfg -D
 curl -fSL -o ../binaries/cockroach-v25.4.2.darwin-11.0-arm64.tgz https://binaries.cockroachdb.com/cockroach-v25.4.2.darwin-11.0-arm64.tgz
 curl -fSL -o ../binaries/cockroach-v25.4.1.darwin-11.0-arm64.tgz https://binaries.cockroachdb.com/cockroach-v25.4.1.darwin-11.0-arm64.tgz
 
-tar -xvf ../binaries/cockroach-v25.4.2.darwin-11.0-arm64.tgz 
+tar -xvf ../binaries/cockroach-v25.4.2.darwin-11.0-arm64.tgz -C ../binaries
 cp ../binaries/cockroach-v25.4.2.darwin-11.0-arm64/cockroach  ../binaries/cockroach-v25.4.2
 
-tar -xvf ../binaries/cockroach-v25.4.1.darwin-11.0-arm64.tgz 
+tar -xvf ../binaries/cockroach-v25.4.1.darwin-11.0-arm64.tgz -C ../binaries
 cp ../binaries/cockroach-v25.4.1.darwin-11.0-arm64/cockroach  ../binaries/cockroach-v25.4.1
